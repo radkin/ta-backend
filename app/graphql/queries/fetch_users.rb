13ca51bf286e9c100f4b538a9aa85b78b486ac6b
Noel Miller <noeldamonmiller@gmail.com>
2023-09-25 14:12:48 -0700

    main refactored into separate methods

--- app/graphql/queries/fetch_users.rb
+++ app/graphql/queries/fetch_users.rb
@@ -2,26 +2,51 @@ module Queries
   class FetchUsers < Queries::BaseQuery
 
     require_relative '../../services/remote_random_user'
-
     require 'json'
 
     type [Types::RandomUserType ], null: false
 
     def resolve(**args)
+      # Fetch the random user data
       rru = RemoteRandomUser.new('https://randomuser.me/api')
       response = rru.gather
-
       data = JSON.parse(response)
       random_user_data = data['results'][0]
 
-      # build the name object
-      this_name = Name.new(
+      # Build the RandomUser object
+      random_user = RandomUser.new(
+        gender: random_user_data["gender"],
+        name: build_name(random_user_data),
+        location: build_location(random_user_data),
+        email: random_user_data["email"],
+        login: build_login(random_user_data),
+        dob: build_dob(random_user_data),
+        registered: build_registered(random_user_data),
+        phone: random_user_data["phone"],
+        cell: random_user_data["cell"],
+        ruid: build_ruid(random_user_data),
+        picture: build_picture(random_user_data),
+        nat: random_user_data["nat"]
+      )
+
+      # Save the RandomUser object to the database
+      random_user.save!
+
+      # Return all RandomUser objects in the database
+      RandomUser.all
+    end
+
+    private
+
+    def build_name(random_user_data)
+      Name.new(
         title: random_user_data["name"]["title"],
         first: random_user_data["name"]["first"],
         last: random_user_data["name"]["last"]
       )
+    end
 
-      # build the location object
+    def build_location(random_user_data)
       street = random_user_data["location"]["street"]
       timezone = random_user_data["location"]["timezone"]
 
@@ -33,7 +58,7 @@ module Queries
       )
       this_timezone = Timezone.new(timezone)
 
-      this_location = Location.new(
+      Location.new(
         street: this_street,
         city: random_user_data["location"]["city"],
         state: random_user_data["location"]["state"],
@@ -42,9 +67,10 @@ module Queries
         coordinate: this_coordinate,
         timezone: this_timezone
       )
+    end
 
-      # build the login object
-      this_login = Login.new(
+    def build_login(random_user_data)
+      Login.new(
         uuid: random_user_data["uuid"],
         username: random_user_data["username"],
         password: random_user_data["password"],
@@ -53,50 +79,35 @@ module Queries
         sha1: random_user_data["sha1"],
         sha256: random_user_data["sha256"]
       )
+    end
 
-      # build dob object
-      this_dob = Dob.new(
+    def build_dob(random_user_data)
+      Dob.new(
         date: random_user_data["dob"]["date"],
         age: random_user_data["dob"]["age"]
       )
+    end
 
-      # build registered object
-      this_registered = Registered.new(
+    def build_registered(random_user_data)
+      Registered.new(
         date: random_user_data["registered"]["date"],
         age: random_user_data["registered"]["age"]
       )
+    end
 
-      # build the random user ID object
-      this_ruid = Ruid.new(
+    def build_ruid(random_user_data)
+      Ruid.new(
         name:random_user_data["id"]["name"],
         value:random_user_data["id"]["value"]
       )
+    end
 
-      # build the picture object
-      this_picture = Picture.new(
+    def build_picture(random_user_data)
+      Picture.new(
         large:random_user_data["picture"]["large"],
         medium:random_user_data["picture"]["medium"],
         thumbnail: random_user_data["picture"]["thumbnail"]
       )
-
-      randomUser = RandomUser.new(
-        gender: random_user_data["gender"],
-        name: this_name,
-        location: this_location,
-        email: random_user_data["email"],
-        login: this_login,
-        dob: this_dob,
-        registered: this_registered,
-        phone: random_user_data["phone"],
-        cell: random_user_data["cell"],
-        ruid: this_ruid,
-        picture: this_picture,
-        nat: random_user_data["nat"]
-      )
-
-      randomUser.save!
-
-      RandomUser.all
     end
 
   end
